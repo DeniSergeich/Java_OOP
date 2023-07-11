87bd8a90878ca8735c82d0d971ab6648ffe879b0
Denis <damned_wolf@mail.ru>
2023-07-11 23:44:15 +0300

    Commit

--- src/main/java/Hero/Warrior.java
+++ src/main/java/Hero/Warrior.java
@@ -3,7 +3,7 @@ package Hero;
 public abstract class Warrior extends BaseHero{
     int armor;
     public Warrior(int x, int y) {
-        super(10,10,2,2,10, x, y);
+        super(30,10,2,2,30, x, y);
         this.armor = 100;
     }
 }
